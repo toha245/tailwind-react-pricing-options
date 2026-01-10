33041616256301746e3a8b3e93b56ed3eb1b68c1
Md Abdul Awal Toha (Personal) <tohamd245@gmail.com>
2026-01-10 18:50:49 +0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import DaisyNav from "./components/DaysyNav/DaisyNav";
 import Navbar from "./components/Navbar/Navbar";
 import PricingOption from "./components/PricingOptions/PricingOption";
 import ResultsChart from "./components/ResultsChart/ResultsChart";
+import axios from "axios";
+import MarksChart from "./components/MarksChart/MarksChart";
 
 const pricingPromise = fetch("pricingData.json").then((res) => res.json());
+
+const marksPromise = axios.get("marksData.json");
 
 function App() {
   return (
@@ -19,6 +23,12 @@ function App() {
           fallback={<span className="loading loading-dots loading-lg"></span>}
         >
           <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
+
+        <Suspense
+          fallback={<span className="loading loading-dots loading-lg"></span>}
+        >
+          <MarksChart marksPromise={marksPromise}></MarksChart>
         </Suspense>
 
         <ResultsChart></ResultsChart>
